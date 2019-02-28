404d6eb6b9ee221371a19441d36d59c93d113319
khtsat <omar.f.bishtawi@gmail.com>
2019-02-28 19:19:15 +0200

    add read/write to file code

--- Bishtawi/Pizza/src/PizzaHashcode.cpp
+++ Bishtawi/Pizza/src/PizzaHashcode.cpp
@@ -7,9 +7,52 @@
 //============================================================================
 
 #include <iostream>
+#include<fstream>
+#include <string>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	/*
+	 * 	Write to file
+	 */
+	ofstream myfile;
+	myfile.open ("example.txt");
+	myfile << "Writing this to a file.\nhi";
+	myfile.close();
+
+
+	string x;
+	ifstream file ("example.txt");
+/*
+ * 	read variable by variable
+ */
+	if (file.is_open())
+	{
+		while ( file >> x  )
+		{
+			cout << x << '\n';
+		}
+		file.close();
+	}else{
+		cout <<"sth wrong";
+	}
+	/*
+	 * 	read line by line
+	 */
+	cout << "----------------" <<endl;
+	string line;
+	ifstream file2 ("example.txt");
+
+	if (file2.is_open())
+	{
+		while ( getline(file2,line)  )
+		{
+			cout << line << '\n';
+		}
+	}
+
+
 	return 0;
+
 }
