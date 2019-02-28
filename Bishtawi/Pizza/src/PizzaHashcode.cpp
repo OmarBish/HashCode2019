f0ffe6cdda7795104f788f306eab9895cb541e3c
khtsat <omar.f.bishtawi@gmail.com>
2019-02-28 18:56:49 +0200

    genesis

---
+++ Bishtawi/Pizza/src/PizzaHashcode.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : PizzaHashcode.cpp
+// Author      : Omat Bishtawi
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
